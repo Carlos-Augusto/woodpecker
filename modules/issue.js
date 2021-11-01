2fc9d62cd0b509e5b649da1fd59036ac8e313854
Carlos Sanchez <caaugustoss@gmail.com>
2021-11-01 21:44:14 +0100

    Adding cwt and signing part and replacement

--- modules/issue.js
+++ modules/issue.js
@@ -1,6 +1,14 @@
 import httpCertify from "./_httpCertify.js";
 
-export default async (stage, body, dccType, locId, txId, pfxFile, passphrase) => {
+import cbor from "cbor";
+import crypto from "crypto";
+import util from "util";
+import zlib from "zlib";
+import base45 from "base45";
+
+const deflatePromise = util.promisify(zlib.deflate);
+
+export default async (stage, data, dccType, locId, txId, pfxFile, passphrase) => {
     const path = "/api/certify/v2/issue/hash";
     const _headers = {
         "x-ubirch-dcctype": dccType,
@@ -9,8 +17,36 @@ export default async (stage, body, dccType, locId, txId, pfxFile, passphrase) =>
         "Content-Type": "text/plain"
     };
 
-    const resp = await httpCertify(stage, path, "post", body, _headers, pfxFile, passphrase);
+    let p = new cbor.Map();
+    p.set(1, data);
 
-    return resp.buffer();
+    let _payload = new cbor.Map();
+    _payload.set(1, "DE"); // issuer
+    _payload.set(6, 1619167131); // issued time
+    _payload.set(4, 1719792666); // exp
+    _payload.set(-260, p);
+
+    let payload = cbor.encodeCanonical(_payload);
+
+    const sigStructureEncoded = cbor.encodeCanonical([
+        'Signature1',
+        Buffer.from([0xA1, 0x01, 0x26]),
+        Buffer.from([]),
+        payload
+    ]);
+
+    const hash = crypto.createHash('sha256').update(sigStructureEncoded).digest('base64');
+
+    const resp = await httpCertify(stage, path, "post", hash, _headers, pfxFile, passphrase);
+    const cert = await resp.buffer();
+
+    const decodedCert = await cbor.decodeAll(cert);
+    decodedCert[0].value[2] = payload;
+
+    const encodedCert = cbor.encodeCanonical(decodedCert[0]);
+    const compressedCert = await deflatePromise(encodedCert);
+
+    return "HC1:" + base45.encode(compressedCert);
+
 };
 
