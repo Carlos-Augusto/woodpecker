a12730d49e1b37bbe35bd097d4bd6d72285d22d3
Carlos Sanchez <caaugustoss@gmail.com>
2021-11-01 07:34:56 +0100

    Adding headers for call

--- modules/issue.js
+++ modules/issue.js
@@ -1,8 +1,15 @@
 import httpCertify from "./_httpCertify.js";
 
-export default async (stage, body, headers, pfxFile, passphrase) => {
-    const path = "/api/certify/v2/issue/hash"
-    const resp = await httpCertify(stage, path, "post", body, headers, pfxFile, passphrase)
+export default async (stage, body, dccType, locId, txId, pfxFile, passphrase) => {
+    const path = "/api/certify/v2/issue/hash";
+    const _headers = {
+        "x-ubirch-dcctype": dccType,
+        "x-location-id": locId,
+        "x-transaction-id": txId,
+        "Content-Type": "text/plain"
+    };
+
+    const resp = await httpCertify(stage, path, "post", body, _headers, pfxFile, passphrase);
 
     return resp.buffer();
 };
