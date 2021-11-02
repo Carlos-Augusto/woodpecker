30d27df8ab0550e078fe6082d389f882e70ec767
Carlos Sanchez <caaugustoss@gmail.com>
2021-11-02 21:57:25 +0100

    Making error more explicit

--- modules/_httpRequest.js
+++ modules/_httpRequest.js
@@ -4,7 +4,8 @@ export default async (request) => {
     const resp = await fetch(request.url, request.details);
 
     if (resp.status > 299) {
-        throw Error("response_status=" + resp.status);
+        const message = await resp.text()
+        throw new Error("response_message=" + message + " response_status=" + resp.status);
     }
 
     return resp;
