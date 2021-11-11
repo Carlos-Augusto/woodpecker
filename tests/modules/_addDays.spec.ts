c85f5480e81d7e147848a564164a71491a7e43a4
Carlos Sanchez <caaugustoss@gmail.com>
2021-11-11 08:15:56 +0100

    Adding tests support

---
+++ tests/modules/_addDays.spec.ts
@@ -0,0 +1,12 @@
+import assert from 'node:assert'
+import { addDays } from '../../modules/_addDays.js'
+import { describe, it } from 'mocha'
+
+describe('_addDays', () => {
+  it('add n days OK', () => {
+    const date = new Date('2021-11-11T23:00:00.000Z')
+    const expectedDate = new Date('2021-11-14T23:00:00.000Z')
+    const days = addDays(date, 3)
+    assert(days.getTime() === expectedDate.getTime())
+  })
+})
