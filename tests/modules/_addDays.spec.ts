ea59367543fca398630a140fdaa768b9a665b131
Carlos Sanchez <caaugustoss@gmail.com>
2021-11-11 09:31:09 +0100

    Adding unix time test

--- tests/modules/_addDays.spec.ts
+++ tests/modules/_addDays.spec.ts
@@ -1,5 +1,5 @@
 import assert from 'node:assert'
-import { addDays } from '../../modules/_addDays.js'
+import { addDays, unixTime } from '../../modules/_addDays.js'
 import { describe, it } from 'mocha'
 
 describe('_addDays', () => {
@@ -8,5 +8,11 @@ describe('_addDays', () => {
     const expectedDate = new Date('2021-11-14T23:00:00.000Z')
     const days = addDays(date, 3)
     assert(days.getTime() === expectedDate.getTime())
+  })
+
+  it('build unix time', () => {
+    const date = new Date('2021-11-11T23:00:00.000Z')
+    const expected = 1636671600
+    assert(unixTime(date.getTime()) === expected)
   })
 })
