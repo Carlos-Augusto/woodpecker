56b439f11a896ec9543c1d467931640555994d06
Carlos Sanchez <caaugustoss@gmail.com>
2021-11-19 08:55:19 +0100

    Adding initial support for verification v2

--- modules/verify.ts
+++ modules/verify.ts
@@ -1,21 +1,29 @@
 import httpVerify from './_httpVerify.js'
 import { HttpCredential, Stage } from './_httpCertify.js'
+import { HeadersInit } from 'node-fetch'
 
 export interface Verify<V> extends HttpCredential {
     stage: Stage,
     data: V,
     txId: string,
     txTag: string,
-    verifyFor: string
+    verifyFor: string,
+    validateFor?: string
 }
 
 export const verify = async (verify: Verify<any>): Promise<string> => {
-  const path = '/api/uve/v1/verify'
-  const headers = {
+  let path = '/api/uve/v1/verify'
+  const headers: HeadersInit = {
     'X-Transaction-Id': verify.txId,
     'X-Transaction-Tag': verify.txTag,
     'X-Verify-For': verify.verifyFor,
     'Content-Type': 'text/plain'
+  }
+
+  // This validation only applies for health DCCs
+  if (verify.validateFor) {
+    path = '/api/uve/v2/verify'
+    headers['X-Validate-For'] = verify.validateFor
   }
 
   const resp = await httpVerify({
