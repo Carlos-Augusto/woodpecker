20dd5b2f4972bfa85108c11926b4256e0189b63a
Carlos Sanchez <caaugustoss@gmail.com>
2021-11-24 23:27:20 +0100

    Adding docs

--- modules/woodpecker.ts
+++ modules/woodpecker.ts
@@ -3,7 +3,7 @@ import issue from './issue.js'
 import { verify } from './verify.js'
 
 /**
- * Principal wrapper or access point for the API.
+ * Principal wrapper or access point for the SDK.
  * It offers, a ping, issue, and verify functions.
  */
 export default { ping, issue, verify }
